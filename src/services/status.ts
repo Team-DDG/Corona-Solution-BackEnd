f57fcd4ebb08d16eef018e801f10a92c8c43128c
HoseungJang <hsjang.dev@gmail.com>
2020-03-19 22:54:41 +0900

    ADD: getHospitalStatus method

--- src/services/status.ts
+++ src/services/status.ts
@@ -17,7 +17,7 @@ export default class StatusService {
                 const sido: string = $(this).find(".cityname").text();
                 const confirmed: string = $(this).find(".cityinfo").children().find("span.num").eq(0).eq(0).text();
                 const dead: string = $(this).find(".cityinfo").children().find("span.num").eq(1).eq(0).text();
-                
+
                 result.push({ sido, confirmed, dead });
             });
 
@@ -34,7 +34,7 @@ export default class StatusService {
             const totals = $("ul.liveNum span.num");
             const befores = $("ul.liveNum span.before");
             const baseDate: string = $("div.live_left span.livedate").text();
-            
+
             const confirmed: IPersonStatus = {
                 total: totals.eq(0).text().split(')')[1],
                 before: befores.eq(0).eq(0).text().split('비 ')[1]
@@ -59,20 +59,45 @@ export default class StatusService {
         }
     };
 
-    public async getClinicStatus({ lat, lng }: ILocationDTO ): Promise<{ baseDate: string, result: IMedicalInstitutionStatus[] }> {
-        const { data } = await axios.get("https://www.mohw.go.kr/react/popup_200128_3.html");
-        const $ = cheerio.load(data);
-        const manager = getManager();
+    public async getClinicStatus({ lat, lng }: ILocationDTO): Promise<{ baseDate: string, result: IMedicalInstitutionStatus[] }> {
+        try {
+            const { data } = await axios.get("https://www.mohw.go.kr/react/popup_200128_3.html");
+            const $ = cheerio.load(data);
+            const manager = getManager();
 
-        const baseDate: string = $("div.viewtop").text().split("황")[1].trim();
-        const clinics = await manager.query(`select *, (6371*acos(cos(radians(${lat}))*cos(radians(lat))*cos(radians(lng)-radians(${lng}))+sin(radians(${lat}))*sin(radians(lat)))) as distance from clinic having distance <= 1 order by distance;`);
-        const result: IMedicalInstitutionStatus[] = [];
+            const baseDate: string = $("div.viewtop").text().split("황")[1].trim();
+            const clinics = await manager.query(`select *, (6371*acos(cos(radians(${lat}))*cos(radians(lat))*cos(radians(lng)-radians(${lng}))+sin(radians(${lat}))*sin(radians(lat)))) as distance from clinic having distance <= 1 order by distance;`);
+            const result: IMedicalInstitutionStatus[] = [];
 
-        clinics.forEach(clinic => {
-            const { name, address, phone, lat, lng } = clinic;
-            result.push({ name, address, phone, lat, lng });
-        });
+            clinics.forEach((clinic: IMedicalInstitutionStatus) => {
+                const { name, address, phone, lat, lng }: IMedicalInstitutionStatus = clinic;
+                result.push({ name, address, phone, lat, lng });
+            });
 
-        return { baseDate, result };
-    }
+            return { baseDate, result };
+        } catch (err) {
+            throw err;
+        }
+    };
+
+    public async getHospitalStatus({ lat, lng }: ILocationDTO): Promise<{ baseDate: string, result: IMedicalInstitutionStatus[] }> {
+        try {
+            const { data } = await axios.get("https://www.mohw.go.kr/react/popup_200128.html");
+            const $ = cheerio.load(data);
+            const manager = getManager();
+
+            const baseDate: string = $("div.viewtop").text().split("황")[1].trim();
+            const hospitals = await manager.query(`select *, (6371*acos(cos(radians(${lat}))*cos(radians(lat))*cos(radians(lng)-radians(${lng}))+sin(radians(${lat}))*sin(radians(lat)))) as distance from hospital having distance <= 1 order by distance;`);
+            const result: IMedicalInstitutionStatus[] = [];
+
+            hospitals.forEach((hospital: IMedicalInstitutionStatus) => {
+                const { name, address, phone, lat, lng }: IMedicalInstitutionStatus = hospital;
+                result.push({ name, address, phone, lat, lng });
+            });
+
+            return { baseDate, result };
+        } catch (err) {
+            throw err;
+        }
+    };
 };
