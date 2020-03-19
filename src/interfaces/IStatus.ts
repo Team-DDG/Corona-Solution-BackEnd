f47a548eb3b952abc649bd828dbd76378a44869d
HoseungJang <hsjang.dev@gmail.com>
2020-03-19 22:41:30 +0900

    ADD: IMedicalInstitution interface

--- src/interfaces/IStatus.ts
+++ src/interfaces/IStatus.ts
@@ -1,18 +1,25 @@
-export interface IStatus {
+export interface IPersonStatus {
     total: string;
     before: string;
-};
+}
 
 export interface ISidoStatus {
     sido: string;
     confirmed: string;
     dead: string;
-};
+}
 
 export interface IPatientStatus {
-    baseDate: string;
-    confirmed: IStatus;
-    cured: IStatus;
-    curing: IStatus
-    dead: IStatus;
-};
+    confirmed: IPersonStatus;
+    cured: IPersonStatus;
+    curing: IPersonStatus
+    dead: IPersonStatus;
+}
+
+export interface IMedicalInstitutionStatus {
+    name: string;
+    address: string;
+    phone: string;
+    lat: number;
+    lng: number;
+}
