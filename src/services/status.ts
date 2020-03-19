c09839f8c29ad5f5dba643941573913fddd4563c
HoseungJang <hsjang.dev@gmail.com>
2020-03-19 16:15:43 +0900

    MODIFY: [] => .eq()

--- src/services/status.ts
+++ src/services/status.ts
@@ -8,13 +8,13 @@ export default class StatusService {
             const { data } = await axios.get("http://ncov.mohw.go.kr/");
             const $ = cheerio.load(data);
             const sidoStatus = $("div.maplist").children().not("#mapAll").not("h3").not(".legend");
-            const baseDate = $("div.live_right span.livedate").text();
+            const baseDate: string = $("div.live_right span.livedate").text();
             const status: ISidoStatus[] = [];
 
             sidoStatus.each(function (i, elem) {
-                const sido = $(this).find(".cityname").text();
-                const confirmed = $(this).find(".cityinfo").children().find("span.num")["0"].children[0].data;
-                const dead = $(this).find(".cityinfo").children().find("span.num")["1"].children[0].data;
+                const sido: string = $(this).find(".cityname").text();
+                const confirmed: string = $(this).find(".cityinfo").children().find("span.num").eq(0).eq(0).text();
+                const dead: string = $(this).find(".cityinfo").children().find("span.num").eq(1).eq(0).text();
                 
                 status.push({ sido, confirmed, dead });
             });
@@ -31,23 +31,23 @@ export default class StatusService {
             const $ = cheerio.load(data);
             const totals = $("ul.liveNum span.num");
             const befores = $("ul.liveNum span.before");
-            const baseDate = $("div.live_left span.livedate").text();
+            const baseDate: string = $("div.live_left span.livedate").text();
             
             const confirmed: IStatus = {
-                total: totals[0].children[1].data,
-                before: befores[0].children[0].data.split('비 ')[1]
+                total: totals.eq(0).text().split(')')[1],
+                before: befores.eq(0).eq(0).text().split('비 ')[1]
             };
             const cured: IStatus = {
-                total: totals[1].children[0].data,
-                before: befores[1].children[0].data
+                total: totals.eq(1).text(),
+                before: befores.eq(1).eq(0).text()
             };
             const curing: IStatus = {
-                total: totals[2].children[0].data,
-                before: befores[2].children[0].data
+                total: totals.eq(2).text(),
+                before: befores.eq(2).eq(0).text()
             }
             const dead: IStatus = {
-                total: totals[3].children[0].data,
-                before: befores[3].children[0].data
+                total: totals.eq(3).text(),
+                before: befores.eq(3).eq(0).text()
             }
 
             return { baseDate, confirmed, cured, curing, dead };
