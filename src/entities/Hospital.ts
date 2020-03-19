a2f43b66b7f2569ede126c64892f17b3d4ac1097
HoseungJang <hsjang.dev@gmail.com>
2020-03-19 22:03:45 +0900

    ADD: Hospital entity

---
+++ src/entities/Hospital.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryColumn, Column } from "typeorm";
+
+@Entity()
+export class Hospital {
+
+    @Column("varchar", { length: 100 })
+    name: string;
+
+    @PrimaryColumn("varchar", { length: 100 })
+    address: string;
+
+    @Column("varchar", { length: 100 })
+    phone: string;
+
+    @Column("decimal", { precision: 20, scale: 17 })
+    lng: string;
+
+    @Column("decimal", { precision: 20, scale: 18 })
+    lat: string;
+}
