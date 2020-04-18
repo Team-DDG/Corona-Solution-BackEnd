9e025ac23e9d2d27ce7c5ba6bffc6e7156e0a3f1
HoseungJang <hsjang.dev@gmail.com>
2020-04-18 20:26:44 +0900

    UPDATE: remove entity import

--- src/loaders/typeorm.ts
+++ src/loaders/typeorm.ts
@@ -1,7 +1,5 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { Clinic } from "../entities/Clinic";
-import { Hospital } from "../entities/Hospital";
 
 export default async () => {
     await createConnection();
