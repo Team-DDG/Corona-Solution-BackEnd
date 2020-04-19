1ce57ceaf42acd7cd4247359939b1a2b892cfbb2
HoseungJang <hsjang.dev@gmail.com>
2020-04-19 14:25:42 +0900

    ADD: ormconfig.js

---
+++ ormconfig.js
@@ -0,0 +1,12 @@
+const { PASSWORD } = process.env;
+
+module.exports = {
+    type: "mysql",
+    host: "janghoseung.cohmubv7xzz8.ap-northeast-2.rds.amazonaws.com",
+    port: 3306,
+    username: "root",
+    password: PASSWORD,
+    database: "corona",
+    synchronize: true,
+    entities: ["src/entities/*.ts"]
+}
