6b27197cc4ca8656e97d52a4d49da887ed12a2e6
HoseungJang <hsjang.dev@gmail.com>
2020-04-21 16:31:28 +0900

    UPDATE: 미들웨어 사이 개행

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -3,11 +3,13 @@ import routes from "../api";
 
 export default ({ app }: { app: Application }) => {
     app.use("/api", routes());
+    
     app.use((req: Request, res: Response, next: NextFunction) => {
         const err = new Error("Not Found");
         err["status"] = 404;
         next(err);
     });
+
     app.use((err: any, req: Request, res: Response, next: NextFunction) => {
         res.sendStatus(err.status || 500);
     });
