73b373dda48a70b5cb841ff4dd63b4eb367e6459
HoseungJang <hsjang.dev@gmail.com>
2020-05-09 20:40:04 +0900

    ADD: type alias

--- test/status.spec.ts
+++ test/status.spec.ts
@@ -2,20 +2,20 @@ import { expect } from "chai";
 import { Container } from "typedi";
 import { createConnection } from "typeorm";
 import StatusService from "../src/services/status";
-import { ISidoStatus, IPatientStatus, IHospitalStatus } from "../src/interfaces/IStatus";
+import { sidoResponse, patientResponse, hospitalResponse } from "../src/types/response";
 
 const statusServiceInstance: StatusService = Container.get(StatusService);
 
 describe('/api/status', async () => {
     it('GET /sido', async () => {
-        const sidoStatus: { baseDate: string, result: ISidoStatus[] } = await statusServiceInstance.getSidoStatus();
+        const sidoStatus: sidoResponse = await statusServiceInstance.getSidoStatus();
         
         expect(sidoStatus.baseDate).to.have.lengthOf.at.least(1);
         expect(sidoStatus.result).to.have.lengthOf(18);
     });
 
     it('GET /patient', async () => {
-        const patientStatus: { baseDate: string, result: IPatientStatus } = await statusServiceInstance.getPatientStatus();
+        const patientStatus: patientResponse = await statusServiceInstance.getPatientStatus();
         
         expect(patientStatus.baseDate).to.have.lengthOf.at.least(1);
         for (const e in patientStatus.result) {
@@ -29,13 +29,13 @@ describe('/api/status', async () => {
     });
 
     it('GET /clinic', async () => {
-        const clinicStatus: { result: IHospitalStatus[] } = await statusServiceInstance.getHospitalStatus({ lat: 37.56, lng: 126.98 }, "clinic");
+        const clinicStatus: hospitalResponse = await statusServiceInstance.getHospitalStatus({ lat: 37.56, lng: 126.98 }, "clinic");
         
         expect(clinicStatus.result[0]).to.have.all.keys("name", "address", "phone", "lat", "lng");
     });
 
     it('GET /hospital', async () => {
-        const hospitalStatus: { result: IHospitalStatus[] } = await statusServiceInstance.getHospitalStatus({ lat: 37.56, lng: 126.98 }, "hospital");
+        const hospitalStatus: hospitalResponse = await statusServiceInstance.getHospitalStatus({ lat: 37.56, lng: 126.98 }, "hospital");
     
         expect(hospitalStatus.result[0]).to.have.all.keys("name", "address", "phone", "lat", "lng");
     });
