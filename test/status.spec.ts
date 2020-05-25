2c548afeaf5927fb654f5d82853cf319c1fe4fbb
HoseungJang <hsjang.dev@gmail.com>
2020-05-25 10:44:18 +0900

    UPDATE: before

--- test/status.spec.ts
+++ test/status.spec.ts
@@ -1,21 +1,25 @@
 import { expect } from "chai";
 import { Container } from "typedi";
-import { createConnection } from "typeorm";
+import { useContainer, createConnection } from "typeorm";
 import StatusService from "../src/services/status";
-import { sidoResponse, patientResponse, hospitalResponse } from "../src/types/response";
-
-const statusServiceInstance: StatusService = Container.get(StatusService);
 
 describe('/api/status', async () => {
+    before(async () => {
+        useContainer(Container);
+        await createConnection();
+    });
+
     it('GET /sido', async () => {
-        const sidoStatus: sidoResponse = await statusServiceInstance.getSidoStatus();
+        const statusServiceInstance: StatusService = Container.get(StatusService);
+        const sidoStatus: Service.SidoResponse = await statusServiceInstance.getSidoStatus();
         
         expect(sidoStatus.baseDate).to.have.lengthOf.at.least(1);
         expect(sidoStatus.result).to.have.lengthOf(18);
     });
 
     it('GET /patient', async () => {
-        const patientStatus: patientResponse = await statusServiceInstance.getPatientStatus();
+        const statusServiceInstance: StatusService = Container.get(StatusService);
+        const patientStatus: Service.PatientResponse = await statusServiceInstance.getPatientStatus();
         
         expect(patientStatus.baseDate).to.have.lengthOf.at.least(1);
         for (const e in patientStatus.result) {
@@ -24,18 +28,16 @@ describe('/api/status', async () => {
         }
     });
 
-    it('typeorm connection', async () => {
-        await createConnection();
-    });
-
     it('GET /clinic', async () => {
-        const clinicStatus: hospitalResponse = await statusServiceInstance.getHospitalStatus({ lat: 37.56, lng: 126.98 }, "clinic");
+        const statusServiceInstance: StatusService = Container.get(StatusService);
+        const clinicStatus: Service.HospitalResponse = await statusServiceInstance.getHospitalStatus({ lat: 37.56, lng: 126.98 }, "clinic");
         
         expect(clinicStatus.result[0]).to.have.all.keys("name", "address", "phone", "lat", "lng");
     });
 
     it('GET /hospital', async () => {
-        const hospitalStatus: hospitalResponse = await statusServiceInstance.getHospitalStatus({ lat: 37.56, lng: 126.98 }, "hospital");
+        const statusServiceInstance: StatusService = Container.get(StatusService);
+        const hospitalStatus: Service.HospitalResponse = await statusServiceInstance.getHospitalStatus({ lat: 37.56, lng: 126.98 }, "hospital");
     
         expect(hospitalStatus.result[0]).to.have.all.keys("name", "address", "phone", "lat", "lng");
     });
